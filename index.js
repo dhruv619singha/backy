1c6a969d1e69ef167a6a4854faf1db10dd806bb9
dhruv619singha <dhruvsingha358@gmail.com>
2024-05-27 21:03:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+    res.send('Hello World!')
+})
+
+app.get('/twitter',(req,res)=>{
+    res.send('Twitter')
+})
+
+app.listen(process.env.PORT,()=> {
+    console.log(`app listening on ${port}`)
+})
